a0c9aeae802e44b7b2606ccf9f04e8f37e68d363
bgcicca <landneco0@gmail.com>
2025-02-28 15:31:22 -0300

    feat: adding treemacs support

---
+++ elisp/init-treemacs.el
@@ -0,0 +1,18 @@
+(use-package treemacs
+  :commands treemacs treemacs-find-file
+  :custom (treemacs-width 40)
+  :hook (treemacs-mode . (lambda () (display-line-numbers-mode -1)))
+  :bind (:map shinmera-global-map
+              ("C-x d" . treemacs)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile)
+
+(use-package treemacs-magit
+  :after treemacs magit)
+
+(use-package treemacs-all-the-icons
+  :after treemacs all-the-icons
+  :config (treemacs-load-theme "all-the-icons"))
+
+(provide 'init-treemacs)
