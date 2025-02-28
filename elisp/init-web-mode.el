f381a87b54776ef757c18322275ccf4042007ff5
bgcicca <landneco0@gmail.com>
2025-02-28 15:30:15 -0300

    feat: adding web mode support

---
+++ elisp/init-web-mode.el
@@ -0,0 +1,19 @@
+(use-package web-mode
+  :commands (web-mode)
+  :mode
+  ("\\.htm\\'" . web-mode)
+  ("\\.html\\'" . web-mode)
+  ("\\.xhtml\\'" . web-mode)
+  ("\\.ctml\\'" . web-mode)
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 4)
+  (web-mode-code-indent-offset 4)
+  (web-mode-style-padding 2)
+  (web-mode-script-padding 2))
+
+(use-package rainbow-mode
+  :commands (rainbow-mode)
+  :hook (css-mode . rainbow-mode))
+
+(provide 'init-web-mode)
